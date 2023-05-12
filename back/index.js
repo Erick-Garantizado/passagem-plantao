36c0adfa51bcfeabaa11187f3e5925914ca1d261
erick <garantizadoerick@gmail.com>
2023-05-12 19:05:50 -0400

    instalado e adicionado o cors

--- back/index.js
+++ back/index.js
@@ -3,10 +3,12 @@ const app = express();
 const routerPrivado = require('./routes/privado')
 const routerPublico = require('./routes/publico');
 const LoginController = require('./controllers/LoginController');
+const cors = require('cors')
+
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-
+app.use(cors())
 app.use('/', routerPublico)
 app.use('/plantao', LoginController.validaToken, routerPrivado)
 
