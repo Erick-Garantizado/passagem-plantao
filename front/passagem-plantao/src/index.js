86424dedfdc8319d8cf3370c380f250acbd63a34
erick <garantizadoerick@gmail.com>
2023-05-12 19:10:22 -0400

    retirado os componentes login e navegacao e adicionado as rotas

--- front/passagem-plantao/src/index.js
+++ front/passagem-plantao/src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Login from './components/Login';
-import Navegacao from './components/Navegacao';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Login from './pages/Login';
+import Area from './pages/Area';
+import Passar from './pages/Passar';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-    <Navegacao/>
-    <Login/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login/>} />
+        <Route path='/area' element={<Area/>} />
+        <Route path='/passagem' element={<Passar/>} />
+      </Routes>
+    </BrowserRouter>
   </>
 );
