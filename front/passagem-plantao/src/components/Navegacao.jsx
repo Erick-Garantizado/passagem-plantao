c671cd4cf4ec84d848884ddcc990165ea384dabd
erick <garantizadoerick@gmail.com>
2023-05-12 19:08:58 -0400

    criado o componente de barra de navegação

--- front/passagem-plantao/src/components/Navegacao.jsx
+++ front/passagem-plantao/src/components/Navegacao.jsx
@@ -1,23 +1,56 @@
-import { AppBar, IconButton, Menu, Toolbar, Typography } from '@mui/material'
+import { AppBar, IconButton, Menu, MenuItem, Toolbar } from '@mui/material'
 import MenuIcon from '@mui/icons-material/Menu'
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Navegacao = () => {
-    
+  // variaveis
+  const [anchorEl, setAnchorEl] = useState(null)
+  const open = Boolean(anchorEl)
+  const navigate = useNavigate()
 
+  // metodos
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+  const handlePassar = () => {
+    handleClose()
+    navigate('/passagem')
+  }
+  
   return (
     <AppBar position='static'>
-        <Toolbar>
-            <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}>
-                
-                <MenuIcon/>
-            </IconButton>
-        </Toolbar>
+      <Toolbar>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+          id="basic-button"
+          aria-controls={open ? 'basic-menu' : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? 'true' : undefined}
+          onClick={handleClick}>
+          <MenuIcon />
+        </IconButton>
+        <Menu
+          id="basic-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          MenuListProps={{
+            'aria-labelledby': 'basic-button',
+          }}
+        >
+          <MenuItem onClick={handlePassar}>Passar plantão</MenuItem>
+          <MenuItem onClick={handleClose}>Receber Plantão</MenuItem>
+          <MenuItem onClick={handleClose}>Listar Plantões</MenuItem>
+        </Menu>
+      </Toolbar>
     </AppBar>
   )
 }
