f677e382fa356a21c951eb89102c91d5196c2432
erick <garantizadoerick@gmail.com>
2025-02-02 16:00:21 -0400

    alinhamento dos elementos para ffacil entendimento do codigo

--- front/passagem-plantao/src/pages/Listar.jsx
+++ front/passagem-plantao/src/pages/Listar.jsx
@@ -1,22 +1,19 @@
 import React, { useState, useEffect } from 'react'
 import Navegacao from '../components/Navegacao'
 import { Container, Typography, Box } from '@mui/material'
-
 import api from '../services/api'
 import CardLista from '../components/CardLista'
 
 const Listar = () => {
   
-  useEffect(() => {
+  useEffect( () => {
     api.get('/plantao/listar')
       .then( ({ data }) => {
-        setPlantao(data.plantao)
-        
+        setPlantao(data.plantao)        
       })
       .catch((e) => {
-        console.log(e)
+        alert(e)
       })
-
   }, [])
 
   const [plantao, setPlantao] = useState([])
@@ -25,23 +22,26 @@ const Listar = () => {
   return (
     <>
       <Navegacao />
-      <Container sx={{ width: '100vw', height: '93vh' }}><br />
+      <Container sx={{ width: '100vw', height: '93vh', display:'flex',
+        flexWrap:'wrap', flexDirection: 'column'
+       }}><br />
         {
-          plantao.length !== 0 ?
-          plantao.map( (dadoPlantao) => (
-            <CardLista plantonista={dadoPlantao.plantonista} 
-            turno={dadoPlantao.turno}
-            observacao={dadoPlantao.observacao}
-            chave={dadoPlantao.id}
+          plantao.length !== 0 ? plantao.map( (dado) => (
+            <CardLista 
+            plantonista={dado.plantonista} 
+            turno={dado.turno}
+            observacao={dado.observacao}
+            chave={dado.id}
             />
-          )) :
-          (<>
-            <Typography variant='h4'>
-              Sem registro de plantao a ser recebido
-            </Typography>
-            <Box className="corpo" ></Box>
-          </>
-            
+          )) 
+          :
+          (
+            <>
+              <Typography variant='h4'>
+                Sem registro de plantao a ser recebido
+              </Typography>
+              <Box className="corpo" ></Box>
+            </>
           )
         }
       </Container>
