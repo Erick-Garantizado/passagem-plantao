ec4d38fcb6df210bfdae4dd323e6d24570e416af
ErickNoteLNX <garantizadoerick@gmail.com>
2025-02-12 12:26:10 -0400

    Adicionado o corpo da pagina

--- front/passagem-plantao/src/pages/MeusPlantoes.jsx
+++ front/passagem-plantao/src/pages/MeusPlantoes.jsx
@@ -1,18 +1,50 @@
 import { useState, useEffect } from 'react'
+import { Typography, Container, Box } from '@mui/material'
+import Navegacao from '../components/Navegacao'
+import CardLista from '../components/CardLista'
 import api from '../services/api'
 
+
 const MeusPlantoes = () => {
-  useEffect(() => {
-    api.get('/')
-  
-    return () => {
-      
-    }
+  useEffect( () => {
+    api.get('plantao/meus')
+    .then(( { data } ) => {
+      setPlantao(data.plantao)
+      console.log(data)
+      .catch((e) => {
+        alert(e)
+      })
+    })
   }, [])
-  
+
+  const [plantao, setPlantao] = useState([])
 
   return (
-    <div>MeusPlantoes</div>
+    <>
+      <Navegacao/>
+      <Container sx={{ width: '100vw', height: '89vh', backgroundColor: 'white',
+      display:'flex', flexWrap:'wrap', flexDirection: 'column'
+       }}><br />
+        {
+          plantao.length !== 0 ? plantao.map( (dado) => (
+            <CardLista 
+            plantonista={dado.plantonista} 
+            turno={dado.turno}
+            observacao={dado.observacao}
+            chave={dado.id}
+            />
+          )) 
+          :
+          (
+            <>
+              <Typography variant='h4'>
+                Sem registro de plantão
+              </Typography>
+            </>
+          )
+        }
+      </Container>
+    </>
   )
 }
 
