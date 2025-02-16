064e81f45702ee2b6de4ab741d84b0722e161db6
erick <garantizadoerick@gmail.com>
2025-02-16 11:17:42 -0400

    Retirada de comentario
    Retirado a antiga barra de navegação que havia sido comentado

--- front/passagem-plantao/src/components/Navegacao.jsx
+++ front/passagem-plantao/src/components/Navegacao.jsx
@@ -7,66 +7,6 @@ import { useNavigate } from 'react-router-dom'
 import api from '../services/api'
 import { AccountCircle } from '@mui/icons-material';
 
-// const Navegacao = () => {
-//   // variaveis
-//   const [anchorEl, setAnchorEl] = useState(null)
-//   const open = Boolean(anchorEl)
-//   const navigate = useNavigate()
-
-//   // metodos
-//   const handleClick = (event) => {
-//     setAnchorEl(event.currentTarget)
-//   }
-//   const handleClose = () => {
-//     setAnchorEl(null)
-//   }
-//   const handlePassar = () => {
-//     handleClose()
-//     navigate('/passagem')
-//   }
-//   const handleListar = () => {
-//     handleClose()
-//     navigate('/listagem')
-//   }
-//   const handleSair = () => {
-//     localStorage.removeItem('user-token')
-//     navigate('/')
-//   }
-  
-//   return (
-//     <AppBar position='static'>
-//       <Toolbar>
-//         <IconButton
-//           size="large"
-//           edge="start"
-//           color="inherit"
-//           aria-label="menu"
-//           sx={{ mr: 2 }}
-//           id="basic-button"
-//           aria-controls={open ? 'basic-menu' : undefined}
-//           aria-haspopup="true"
-//           aria-expanded={open ? 'true' : undefined}
-//           onClick={handleClick}>
-//           <MenuIcon />
-//         </IconButton>
-//         <Menu
-//           id="basic-menu"
-//           anchorEl={anchorEl}
-//           open={open}
-//           onClose={handleClose}
-//           MenuListProps={{
-//             'aria-labelledby': 'basic-button',
-//           }}
-//         >
-//           <MenuItem onClick={handlePassar}>Passar plantão</MenuItem>
-//           <MenuItem onClick={handleListar} divider>Receber Plantão</MenuItem>
-//           <MenuItem onClick={handleSair}>Sair</MenuItem>
-//         </Menu>
-//       </Toolbar>
-//     </AppBar>
-//   )
-// }
-
 
 const Navegacao = () => {
 
