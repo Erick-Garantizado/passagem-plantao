45e00b283878556b72b3daf934250e47eb58436d
ErickNoteLNX <garantizadoerick@gmail.com>
2025-02-24 18:48:48 -0400

    inserido botao no lugar do boleano

--- front/passagem-plantao/src/pages/Detalhes.jsx
+++ front/passagem-plantao/src/pages/Detalhes.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react'
-import Navegacao from '../components/Navegacao'
-import stilo from "./Area.css"
-import { Container, Typography } from '@mui/material'
+//import Navegacao from '../components/Navegacao'
+import { Container, Typography, Button } from '@mui/material'
 import { useParams } from 'react-router-dom';
 import api from '../services/api'
 
@@ -24,12 +23,18 @@ const Detalhes = () => {
   return (
     <>
       <Container sx={{ width: '100vw', height: '100vh' }} >
-        <Typography> ID: {plantao.id} </Typography><br/>
-        <Typography> Plantonista 1: {plantao.id_pass} </Typography>
-        <Typography> Plantonista 2: {plantao.id_receb} </Typography>
+        <Typography> Plantonista : {plantao.id_pass} </Typography>
+        <Typography> Receptor(a) : {plantao.id_receb} </Typography>
         <Typography> Observação: {plantao.observacao} </Typography>
-        <Typography> Situação: { String(plantao.situacao)} </Typography>
         <Typography> Turno: {plantao.turno} </Typography>
+        <Typography> Situação: 
+          {
+            plantao.situacao === false ? 
+            ( <Button variant="contained" color="error" size="small" >Pendente</Button> ) :
+            ( <Button variant="contained" color="success">OK</Button> )
+          } 
+        </Typography>
+
       </Container>
     </>
   )
