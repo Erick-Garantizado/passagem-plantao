161008ca793a30b00c7fe6a139a7a8a628bcfb63
erick-pc <garantizadoerick@gmail.com>
2025-11-10 21:01:43 +0000

    Alteração da primeira letra do arquivo para maiúscula

--- front/passagem-plantao/src/pages/Cadastro.jsx
+++ front/passagem-plantao/src/pages/Cadastro.jsx
@@ -3,16 +3,16 @@ import { useForm, Controller } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import api from "../services/api";
-import { 
-  TextField, FormControl, FormLabel, RadioGroup, FormControlLabel, Radio, 
-  Checkbox, Select, MenuItem, Button, FormHelperText 
-} from "@mui/material";
+import { TextField, FormControl, FormLabel, RadioGroup, FormControlLabel, Radio, 
+  Checkbox, Select, MenuItem, Button, FormHelperText } from "@mui/material";
+import Navegacao from "../components/Navegacao";
 
 // Esquema de validação com Yup
 const schema = yup.object({
   nome: yup.string().required("O nome é obrigatório"),
   email: yup.string().email("E-mail inválido").required("O e-mail é obrigatório"),
-  genero: yup.string().required("Escolha um gênero"),
+  matricula: yup.string().required("A matricula é obrigatória"),
+  profissao: yup.string().required("A profissão é obrigatória"),
 });
 
 const Cadastro = () => {
@@ -21,110 +21,99 @@ const Cadastro = () => {
     defaultValues: {
       nome: "",
       email: "",
-      genero: "",
+      matricula: "",
+      profissao: "",
+      permissao: ""
     },
   });
 
   const onSubmit = (data) => {
-    api.post('/create')
-    // nome
-    // funcao
-    // matricula
-    // email
+    data.permissao = parseInt(data.permissao)
+
+    api.post('plantao/usuario/criar', {
+      nome: data.nome,
+      email: data.email,
+      matricula: data.matricula,
+      permissao: data.permissao,
+      funcao: data.profissao
+    })
+    .then(()=>{alert("Usuario criado!")})
+    .catch((e)=>{console.log(e)})
   };
 
-  const handleClick = () => {
-    api('enviado')
-  }
-
   return (
-    <form onSubmit={handleSubmit(onSubmit)} style={{ maxWidth: 400, margin: "auto", padding: 20 }}>
-      {/* Nome */}
-      <Controller
-        name="nome"
-        control={control}
-        render={({ field }) => (
-          <TextField {...field} label="Nome" fullWidth variant="standard"
-          margin="normal" error={!!errors.nome} helperText={errors.nome?.message} />
-        )}
-      />
-
-      {/* Email */}
-      <Controller
-        name="email"
-        control={control}
-        render={({ field }) => (
-          <TextField {...field} label="E-mail" fullWidth variant="standard"
-          margin="normal" error={!!errors.email} helperText={errors.email?.message} />
-        )}
-      />
-      
-      {/* Matricula */}
-      <Controller
-        name="matricula"
-        control={control}
-        render={({ field }) => (
-          <TextField {...field} label="Matricula" fullWidth variant="standard"
-          margin="normal" error={!!errors.email} helperText={errors.email?.message} />
-        )}
-      />
-
-      {/* Gênero (Radio) */}
-      <FormControl margin="normal" error={!!errors.genero}>
-        <FormLabel>Gênero de nasc.</FormLabel>
+    <>
+      <Navegacao />
+      <form onSubmit={handleSubmit(onSubmit)} style={{ maxWidth: 400, margin: "auto", padding: 20 }}>
+        {/* Nome */}
         <Controller
-          name="genero"
+          name="nome"
           control={control}
           render={({ field }) => (
-            <RadioGroup {...field}>
-              <FormControlLabel value="masculino" control={<Radio />} label="Masculino" />
-              <FormControlLabel value="feminino" control={<Radio />} label="Feminino" />
-            </RadioGroup>
+            <TextField {...field} label="Nome" fullWidth variant="standard"
+            margin="normal" error={!!errors.nome} helperText={errors.nome?.message} />
           )}
         />
-        <FormHelperText>{errors.genero?.message}</FormHelperText>
-      </FormControl>
 
-      {/* Interesses (Checkbox) */}
-      {/* <FormControl margin="normal" error={!!errors.interesses}>
-        <FormLabel>Interesses</FormLabel>
+        {/* Email */}
         <Controller
-          name="interesses"
+          name="email"
           control={control}
           render={({ field }) => (
-            <>
-              <FormControlLabel control={<Checkbox onChange={e => field.onChange(e.target.checked ? [...field.value, "Música"] : field.value.filter(v => v !== "Música"))} />} label="Música" />
-              <FormControlLabel control={<Checkbox onChange={e => field.onChange(e.target.checked ? [...field.value, "Esportes"] : field.value.filter(v => v !== "Esportes"))} />} label="Esportes" />
-              <FormControlLabel control={<Checkbox onChange={e => field.onChange(e.target.checked ? [...field.value, "Tecnologia"] : field.value.filter(v => v !== "Tecnologia"))} />} label="Tecnologia" />
-            </>
+            <TextField {...field} label="E-mail" fullWidth variant="standard"
+            margin="normal" error={!!errors.email} helperText={errors.email?.message} />
           )}
         />
-        <FormHelperText>{errors.interesses?.message}</FormHelperText>
-      </FormControl> */}
-
-      {/* País (Select) */}
-      <FormControl variant="standard" fullWidth margin="normal" error={!!errors.pais}>
-        <FormLabel>Profissão</FormLabel>
+        
+        {/* Matricula */}
         <Controller
-          name="profissao"
+          name="matricula"
           control={control}
           render={({ field }) => (
-            <Select {...field} displayEmpty>
-              <MenuItem value="">Selecione uma profissão</MenuItem>
-              <MenuItem value="medico">Médico</MenuItem>
-              <MenuItem value="enfermeiro">Enfermeiro</MenuItem>
-              <MenuItem value="tec_enfermagem">Técnico em enfermagem</MenuItem>
-            </Select>
+            <TextField {...field} label="Matricula" fullWidth variant="standard"
+            margin="normal" error={!!errors.email} helperText={errors.email?.message} />
           )}
         />
-        <FormHelperText>{errors.pais?.message}</FormHelperText>
-      </FormControl>
 
-      {/* Botão de Envio */}
-      <Button type="submit" variant="contained" color="primary" fullWidth sx={{ mt: 2 }}      >
-        Enviar
-      </Button>
-    </form>
+        {/* Gênero (Radio) */}
+        <FormControl margin="normal" error={!!errors.permissao}>
+          <FormLabel>Permissão</FormLabel>
+          <Controller
+            name="permissao"
+            control={control}
+            render={({ field }) => (
+              <RadioGroup {...field}>
+                <FormControlLabel value='0' control={<Radio />} label="Administrador" />
+                <FormControlLabel value='1' control={<Radio />} label="Usuario" />
+              </RadioGroup>
+            )}
+          />
+          <FormHelperText>{errors.permissao?.message}</FormHelperText>
+        </FormControl>
+
+        {/* Profissao (Select) */}
+        <FormControl variant="standard" fullWidth margin="normal" error={!!errors.profissao}>
+          <FormLabel>Profissão</FormLabel>
+          <Controller
+            name="profissao"
+            control={control}
+            render={({ field }) => (
+              <Select {...field} displayEmpty>
+                <MenuItem value="medico">Médico(a)</MenuItem>
+                <MenuItem value="enfermeiro">Enfermeiro(a)</MenuItem>
+                <MenuItem value="tec_enfermagem">Técnico(a) em enfermagem</MenuItem>
+              </Select>
+            )}
+          />
+          <FormHelperText>{errors.profissao?.message}</FormHelperText>
+        </FormControl>
+
+        {/* Botão de Envio */}
+        <Button type="submit" variant="contained" color="primary" fullWidth sx={{ mt: 2 }}      >
+          Enviar
+        </Button>
+      </form>
+    </>
   );
 }
 
