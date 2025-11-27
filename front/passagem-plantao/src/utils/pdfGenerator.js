e5beb167e84afb3aa529dc734cf1532d0bf692c1
Erick Garantizado <garantizadoerick@gmail.com>
2025-11-27 15:45:02 +0000

    criação do arquivo que cria o pdf

--- front/passagem-plantao/src/utils/pdfGenerator.js
+++ front/passagem-plantao/src/utils/pdfGenerator.js
@@ -1,15 +1,32 @@
-import pdfMake from 'pdfmake/build/pdfmake';
-import pdfFonts from 'pdfmake/build/vfs_fonts';
-pdfMake.vfs = pdfFonts.pdfMake.vfs;
+import pdfMake from './pdfConfig'
 
-const docDefinition = {
-  content: [
-    { text: 'Relatório', style: 'header' },
-    { text: `Data: ${new Date().toLocaleDateString()}` }
-  ],
-  styles: {
-    header: { fontSize: 18, bold: true }
-  }
-};
 
-pdfMake.createPdf(docDefinition).open();
+export const gerarPDF = () => {
+    const docDefinition = {
+        content: [
+            { text: "Relatório de Vendas", style: "header" },
+            { text: `Data: ${new Date().toLocaleDateString()}`, style: "subheader" },
+            { text: "-------------------------------------" },
+            { text: "Itens vendidos:" },
+            {
+                table: {
+                widths: ["*", "*", "*"],
+                body: [
+                    ["Produto", "Quantidade", "Preço"],
+                    ["Caneta", "10", "R$ 5,00"],
+                    ["Caderno", "5", "R$ 15,00"],
+                    ["Mochila", "2", "R$ 120,00"],
+                ],
+                },
+            },
+        ],
+        styles: {
+            header: { fontSize: 18, bold: true },
+            subheader: { fontSize: 12, italics: true },
+        },
+        defaultStyle: {
+            fontSize: 11,
+        },
+    }
+    pdfMake.createPdf(docDefinition).open()
+}
