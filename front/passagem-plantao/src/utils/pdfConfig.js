b37935210f3a59984f2302a5aecf276a780384d5
Erick Garantizado <garantizadoerick@gmail.com>
2025-11-27 15:45:32 +0000

    arquivo de configuração do pdfmake

---
+++ front/passagem-plantao/src/utils/pdfConfig.js
@@ -0,0 +1,8 @@
+import pdfMake from "pdfmake/build/pdfmake";
+import pdfFonts from "pdfmake/build/vfs_fonts";
+
+// REGISTRA as fontes globalmente uma vez
+// pdfMake.vfs = pdfFonts.pdfMake.vfs;
+
+
+export default pdfMake;
