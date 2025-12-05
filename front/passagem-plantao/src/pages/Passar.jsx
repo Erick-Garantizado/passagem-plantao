736533695c3ce16606f53c6e00d31de002850c96
Erick Garantizado <garantizadoerick@gmail.com>
2025-12-05 15:41:04 +0000

    Ajuste visual no passar plantão
    Retirado o finally do api.post;
    Retirado o backgroundColor do container e ajustado height para 88 e retirado o boxshadows;
    adicionado um divider, ícones de sol e lua nos turnos;
    Adicionado card no formulário;
    Ajuste no textfield e no loading button

--- front/passagem-plantao/src/pages/Passar.jsx
+++ front/passagem-plantao/src/pages/Passar.jsx
@@ -1,13 +1,16 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import Navegacao from '../components/Navegacao'
 import { Alert, Box, Container, FormControlLabel,
-        Radio, RadioGroup, TextField, 
-        Typography} from '@mui/material'
+        Radio, RadioGroup, TextField, Card, CardContent,
+        Typography,
+        Divider} from '@mui/material'
 import { LoadingButton } from '@mui/lab'
 import api from '../services/api'
 import { useNavigate } from 'react-router-dom'
 import { FormControl, Snackbar } from '@mui/base'
 
+import WbSunnyIcon from "@mui/icons-material/WbSunny";
+import NightsStayIcon from "@mui/icons-material/NightsStay";
 
 const Passar = () => {
     const navigate = useNavigate()
@@ -19,8 +22,7 @@ const Passar = () => {
     
 
     const handleClick = () => {
-        setLoading(true)
-        
+        setLoading(true)        
         api.post('/plantao/salvar', {
             turno : turno.trim(),
             observacao: observacao
@@ -30,14 +32,11 @@ const Passar = () => {
                 navigate('/listagem')
             }, 2000)
         }).catch((e) => {
-            e.message === "Network Error" ? 
-            setError("Erro de conexão com banco.") : 
-            setError(e.message)
-        }).finally(()=>{
-            
-            setLoading(false)
-            setSucesso(false)
+            e.message === "Network Error" 
+            ? setError("Erro de conexão com banco.")
+            : setError(e.message)
         })
+        
     }
 
     const handleChange = (event) => {
@@ -47,9 +46,11 @@ const Passar = () => {
     return (
         <>
           <Navegacao />
-          <Container sx={{ width:'100vw', height:'89vh', backgroundColor: 'white', 
-            display:'flex', flexDirection:'column', alignItems: 'center', boxShadow:10
-          }} >
+          <Container
+           sx={{ width:'100vw', height:'88vh', 
+            display:'flex', flexDirection:'column', alignItems: 'center', mt:2
+          }}
+          >
             <Snackbar open={sucesso} anchorOrigin={{ vertical: 'top', horizontal: 'center' }}>
                 <Alert severity='success' sx={{ width: '100%' }}>
                     Dados salvos!
@@ -60,48 +61,62 @@ const Passar = () => {
                     {error}
                 </Alert>
             </Snackbar>
-            <Typography variant='h4' mt={3} mb={5}>
-              Informações sobre o plantão
-            </Typography>
-            <FormControl >
-              <Box sx={{ display: 'flex', flexDirection:'column', 
-                justifyContent: 'center', alignItems:'center'}} >
-                <Box sx={{ display: 'flex', justifyContent: 'center'}}>
-                  <RadioGroup
-                    sx={{ flexDirection: 'row',  }}
-                    aria-labelledby="radio-buttons-group-label"
-                    defaultValue="diurno"
-                    name="radio-buttons-group"
-                  >
-                    <FormControlLabel value="diurno" label="Diurno" control={
-                        <Radio value='diurno' checked={turno === 'diurno'} onChange={handleChange}/>
-                    }/>
-                    <FormControlLabel value="noturno"  label="Noturno" control={
-                        <Radio value='noturno' checked={turno === 'noturno'} onChange={handleChange}/>
-                    }/>
-                  </RadioGroup>
-                </Box>
-                <Box>
-                  <TextField aria-colcount={5} 
-                  multiline rows={7}  
-                  label='Observação do Plantão' 
-                  sx={{marginTop: '10px', marginBottom: '10px', width:'45vw'}}
-                  onChange={(e)=>setObservacao(e.target.value)}
-                  value={observacao}
-                  />
-                </Box>
-                <Box>
-                  <LoadingButton
-                  loading={loading} 
-                  color="primary" 
-                  variant="contained" 
-                  onClick={handleClick}
-                  >
-                      Enviar
-                  </LoadingButton>
-                </Box>
-              </Box>
-            </FormControl>
+            <Card sx={{ width: "60vw", mt: 4, p: 3, boxShadow: 6, borderRadius: 3 }}>
+              <CardContent>
+                <Typography variant='h4' mt={4} mb={2} fontWeight="bold" textAlign='center'> 
+                  Informações do plantão
+                </Typography>
+                <Divider sx={{ my: 3 }} />
+                <FormControl >
+                  <Box sx={{ display: 'flex', flexDirection:'column', 
+                    justifyContent: 'center', alignItems:'center'}} >
+                    <Box sx={{ display: 'flex', justifyContent: 'center'}}>
+                      <RadioGroup
+                        sx={{ flexDirection: 'row'}}
+                        aria-labelledby="radio-buttons-group-label"
+                        defaultValue="diurno"
+                        name="radio-buttons-group"
+                      >
+                        <FormControlLabel sx={{mr:3}} value="diurno" label={<><WbSunnyIcon /> Diurno</>} control={
+                            <Radio value='diurno' checked={turno === 'diurno'} onChange={handleChange}/>
+                        }/>
+                        <FormControlLabel sx={{ml:3}} value="noturno"  label={<><NightsStayIcon /> Noturno</>} control={
+                            <Radio value='noturno' checked={turno === 'noturno'} onChange={handleChange}/>
+                        }/>
+                      </RadioGroup>
+                    </Box>
+                    <Box>
+                      <TextField aria-colcount={5} 
+                      multiline rows={7}  
+                      label='Observação do Plantão' 
+                      sx={{mt:2, mb:3, width:'45vw', borderRadius:2, 
+                        "& .MuiOutlinedInput-root": {borderRadius:2, backgroundColor:'#fafafa'}
+                      }}
+                      onChange={(e)=>setObservacao(e.target.value)}
+                      value={observacao}
+                      />
+                    </Box>
+                    <Box>
+                      <LoadingButton
+                      loading={loading} 
+                      color="primary" 
+                      variant="contained" 
+                      onClick={handleClick}
+                      sx={{
+                        px: 4,
+                        py: 1.3,
+                        borderRadius: 2,
+                        fontSize: "1.1rem",
+                        boxShadow: 3
+                      }}
+                      >
+                          Enviar
+                      </LoadingButton>
+                    </Box>
+                  </Box>
+                </FormControl>
+              </CardContent>
+            </Card>
           </Container>
         </>
     )
