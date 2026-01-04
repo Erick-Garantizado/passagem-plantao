9215c37978c0fac3420fb697a0309a4155b4fd65
erick-pc <garantizadoerick@gmail.com>
2026-01-04 13:48:35 +0000

    Personalização da pag. de erro

--- front/passagem-plantao/src/pages/Erro.jsx
+++ front/passagem-plantao/src/pages/Erro.jsx
@@ -1,8 +1,34 @@
-export default function Erro() {
+import { Box, Button, Typography } from "@mui/material"; 
+import { useNavigate } from "react-router-dom"; 
+
+
+const BACKGROUND_IMAGE = "https://cdn.vectorstock.com/i/500p/45/29/login-access-denied-error-message-vector-58014529.jpg";
+
+const AccessDeniedPage = () => {
+  const navigate = useNavigate();
+  localStorage.removeItem('user-token')
+
+
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Acesso negado</h1>
-      <p>Você não tem permissão para acessar esta página.</p>
-    </div>
+    <Box sx={{ minHeight: "100vh", display: "flex", flexDirection: "column",
+        alignItems: "center", justifyContent: "center", px: 2, textAlign: "center",
+        bgcolor: "#ffffffff",
+      }}
+    >
+      <Box component="img" src={ BACKGROUND_IMAGE } alt="Acesso Negado"
+        sx={{ maxWidth: 500, width: "100%", mb: 4 }}
+      />
+      <Typography variant="h4" gutterBottom>
+        Acesso Negado
+      </Typography>
+      <Typography variant="body1" sx={{ mb: 4 }}>
+        Você não tem permissão para acessar esta página.
+      </Typography>
+      <Button variant="contained" color="primary" onClick={() => navigate("/")}>
+        Voltar para a Página Inicial
+      </Button>
+    </Box>
   );
-}
+};
+
+export default AccessDeniedPage;
