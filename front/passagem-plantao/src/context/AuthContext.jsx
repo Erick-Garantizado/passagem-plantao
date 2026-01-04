957475fab7a66e3b29f930d8cae366c8f685000b
erick-pc <garantizadoerick@gmail.com>
2026-01-04 13:46:16 +0000

    Criação do AuthContext

---
+++ front/passagem-plantao/src/context/AuthContext.jsx
@@ -0,0 +1,35 @@
+// src/context/AuthContext.jsx
+import { createContext, useContext, useState, useEffect } from "react";
+import api from "../services/api";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const loadUser = async () => {
+      try {
+        // tenta buscar o usuário atual (token no localStorage)
+        const { data } = await api.get("plantao/usuario/atual");
+        setUser(data.usuario);
+        console.log(data)
+      } catch {
+        setUser(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    loadUser();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, setUser, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
