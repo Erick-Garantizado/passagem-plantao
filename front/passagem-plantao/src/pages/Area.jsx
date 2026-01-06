cbef2810b78bbc264056085b7e4d4ed0d1304e51
Erick Garantizado <garantizadoerick@gmail.com>
2026-01-06 16:27:43 +0000

    dashboard provisorio

--- front/passagem-plantao/src/pages/Area.jsx
+++ front/passagem-plantao/src/pages/Area.jsx
@@ -1,15 +1,39 @@
 import Navegacao from '../components/Navegacao'
 import { Container } from '@mui/material'
 
+import { Grid, Paper, Typography } from "@mui/material";
+
+function Card({ title, value }) {
+  return (
+    <Paper sx={{ p: 2 }}>
+      <Typography variant="h6">{title}</Typography>
+      <Typography variant="h4">{value}</Typography>
+    </Paper>
+  );
+}
 
 const Area = () => {
 
   return (
     <>
       <Navegacao />
-      <Container sx={{ width: '100vw', height: '93vh' }} >
+      {/* <Container sx={{ width: '100vw', height: '93vh' }} >
         <div className="corpo" ></div>
-      </Container>
+      </Container> */}
+      <br />
+      <Grid container spacing={3}>
+      <Grid item xs={12} md={4}>
+        <Card title="Criticos" value="25" />
+      </Grid>
+
+      <Grid item xs={12} md={4}>
+        <Card title="Pendentes" value="2" />
+      </Grid>
+      
+      <Grid item xs={12} md={4}>
+        <Card title="Total" value="30" />
+      </Grid>      
+    </Grid>
     </>
   )
 }
