9687a01b6d4f62e8c247994c47790158cb3c76ce
Erick Garantizado <garantizadoerick@gmail.com>
2026-01-16 15:16:25 +0000

    Atualização de middleware
    Substituido a Logincontroller por validatoken encontrado agora na pasta de middleware

--- back/index.js
+++ back/index.js
@@ -2,15 +2,15 @@ const express = require('express');
 const app = express();
 const routerPrivado = require('./routes/privado')
 const routerPublico = require('./routes/publico');
-const LoginController = require('./controllers/LoginController');
-const cors = require('cors')
+const { validaToken } = require('./middlewares/authMiddleware');
+const cors = require('cors');
 
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cors())
 app.use('/', routerPublico)
-app.use('/plantao', LoginController.validaToken, routerPrivado)
+app.use('/plantao', validaToken, routerPrivado)
 
 app.listen(process.env.PORT, () => {
     console.log(`Servidor rodando em http://localhost:${process.env.PORT}`)
